ba27d5540acd49c95583ac7f4dd29717ef6bb99b
Latikavlk1152 <116451526+Latikavlk1152@users.noreply.github.com>
2023-11-07 07:56:15 +0530

    Update fibonacci number.java

--- fibonacci number.java
+++ fibonacci number.java
@@ -22,3 +22,23 @@ public class fibonacci {
 };
 
 // This code is contributed by Mihir Joshi
+
+// Fibonacci Series using Recursion
+import java.io.*;
+class fibonacci {
+	static int fib(int n)
+	{
+		if (n <= 1)
+			return n;
+		return fib(n - 1) + fib(n - 2);
+	}
+
+	public static void main(String args[])
+	{
+		int n = 9;
+		System.out.println(
+			n + "th Fibonacci Number: " + fib(n));
+	}
+}
+/* This code is contributed by Rajat Mishra */
+
